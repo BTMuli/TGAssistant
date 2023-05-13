cc59ef19109f8c9ffbd533c86b3e39e351dbe257
BTMuli <BT-Muli@outlook.com>
2023-05-13 22:10:22 +0800

    ♻️ 完成 GCG 资源下载&处理

---
+++ src/gcg/download.js
@@ -0,0 +1,119 @@
+/**
+ * @file gcg download.js
+ * @description 下载 GCG 相关 JSON 和图片
+ * @author BTMuli<bt-muli@outlook.com>
+ * @since 1.1.0
+ */
+
+// Node
+import path from "node:path";
+import fs from "node:fs";
+import log4js from "log4js";
+import axios from "axios";
+import sharp from "sharp";
+// TGAssistant
+import logger from "../tools/logger.js";
+import pathList from "../../root.js";
+import { fileCheck, fileExist } from "../tools/utils.js";
+
+const consoleLog = log4js.getLogger("console");
+logger.info("[GCG][下载] 正在运行 download.js");
+
+const srcImgDir = path.resolve(pathList.src.img, "gcg");
+const srcJsonDir = path.resolve(pathList.src.json, "gcg");
+const amberUrl = "https://api.ambr.top/v2/chs/gcg?vh=37F8";
+const amberSavePath = path.resolve(srcJsonDir, "amber.json");
+const mysUrl = "https://api-static.mihoyo.com/common/blackboard/ys_obc/v1/home/content/list?app_sn=ys_obc&channel_id=231";
+const mysSavePath = path.resolve(srcJsonDir, "mys.json");
+
+// 检测目录是否存在
+fileCheck(srcImgDir);
+fileCheck(srcJsonDir);
+
+// 下载JSON
+if (!fileExist(amberSavePath)) {
+	try {
+		await axios.get(amberUrl, {
+			timeout: 10000
+		}).then(res => {
+			const dataGet = res.data["data"]["items"];
+			const savePath = path.resolve(srcJsonDir, "amber.json");
+			fs.writeFileSync(savePath, JSON.stringify(dataGet, null, 2));
+			logger.info("[GCG][下载] amber.json 下载完成");
+		});
+	} catch (error) {
+		logger.error("[GCG][下载] amber.json 下载失败");
+		logger.error(error.message);
+	}
+} else {
+	consoleLog.info("[GCG][下载] amber.json 已存在，跳过下载");
+}
+if (!fileExist(mysSavePath)) {
+	try {
+		await axios.get(mysUrl).then(res => {
+			const dataGet = res.data["data"]["list"][0]["children"];
+			const savePath = path.resolve(srcJsonDir, "mys.json");
+			fs.writeFileSync(savePath, JSON.stringify(dataGet, null, 2));
+			logger.info("[GCG][下载] mys.json 下载完成");
+		});
+	} catch (error) {
+		logger.error("[GCG][下载] mys.json 下载失败");
+		logger.error(error.message);
+	}
+} else {
+	consoleLog.info("[GCG][下载] mys.json 已存在，跳过下载");
+}
+// 下载图片
+const amberJson = JSON.parse(fs.readFileSync(amberSavePath, "utf-8"));
+const amberKeys = Object.keys(amberJson);
+await Promise.allSettled(amberKeys.map(async (key) => {
+	const item = amberJson[key];
+	const url = getAmberImgUrl(item["icon"]);
+	await downloadImg(url, item["name"], "amber");
+}));
+const mysJson = JSON.parse(fs.readFileSync(mysSavePath, "utf-8"));
+await Promise.allSettled(mysJson.map(async itemList => {
+	await Promise.allSettled(itemList.list.map(async item => {
+		const url = item["icon"];
+		await downloadImg(url, item["title"], "mys");
+	}));
+}));
+
+logger.info("[GCG][下载] download.js 运行结束");
+
+// 用到的函数
+
+/**
+ * @description 获取 amber.json 中的图片下载链接
+ * @param {string} icon 图片名
+ * @returns {string} 图片下载链接
+ */
+function getAmberImgUrl(icon) {
+	return `https://api.ambr.top/assets/UI/gcg/${icon}.png`;
+}
+
+/**
+ * @description 下载图片
+ * @param {string} url 图片下载链接
+ * @param {string} name 图片名
+ * @param {string} source 图片来源
+ * @returns {Promise<void>} 无返回值
+ */
+async function downloadImg(url, name, source) {
+	const savePath = path.resolve(srcImgDir, `${name}.png`);
+	if (fileExist(savePath)) {
+		consoleLog.info(`[GCG][下载][${source}] ${name}.png 已存在，跳过下载`);
+		return;
+	}
+	await axios.get(url, {
+		responseType: "arraybuffer"
+	}).then(res => {
+		sharp(res.data).png().toFile(savePath, (err) => {
+			if (err) {
+				logger.error(`[GCG][下载][${source}] ${name}.png 下载失败`);
+			} else {
+				logger.info(`[GCG][下载][${source}] ${name}.png 下载完成，大小为 ${res.data.length} 字节`);
+			}
+		});
+	});
+}
