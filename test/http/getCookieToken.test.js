521c8e3ab140ed40f46eace35a74cc98999efcda
BTMuli <BT-Muli@outlook.com>
2023-05-19 10:48:40 +0800

    🎨 边界测试

--- test/http/getCookieToken.test.js
+++ test/http/getCookieToken.test.js
@@ -10,13 +10,17 @@ import { describe, it } from "mocha";
 import assert from "node:assert";
 // TGAssistant
 import { getCookieTokenBySToken } from "../../http/request/getCookieToken.js";
-import { readCookie, readCookieItem } from "../../http/tools/readCookie.js";
+import { readCookieItem } from "../../http/tools/readCookie.js";
+import { transCookie } from "../../http/tools/utils.js";
 
 describe("测试 cookieToken 获取", ()=>{
 	it("通过 stoken", async ()=>{
 		const stoken = readCookieItem("stoken");
-		const cookie = readCookie();
-		const res = await getCookieTokenBySToken(cookie, stoken);
+		const cookie = {
+			stoken: stoken,
+			mid: readCookieItem("mid"),
+		};
+		const res = await getCookieTokenBySToken(transCookie(cookie), stoken);
 		const cookieToken = readCookieItem("cookie_token");
 		assert.strictEqual(res, cookieToken);
 	});
