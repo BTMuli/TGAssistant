c367ab4683fd5341289d1065a17ac13548cf2209
BTMuli <BT-Muli@outlook.com>
2023-06-28 09:54:11 +0800

    🎨 逻辑变更

--- http/tools/readCookie.js
+++ http/tools/readCookie.js
@@ -2,7 +2,7 @@
  * @file http tools readCookie.js
  * @description Cookie 读取
  * @author BTMuli<bt-muli@outlook.com>
- * @since 1.1.0
+ * @since 1.2.0
  */
 
 // Node
@@ -11,24 +11,24 @@ import path from "node:path";
 // TGAssistant
 import pathList from "../../root.js";
 
-const ckFile = fs.readFileSync(path.resolve(pathList.http, "cookie.json"), "utf8");
-
 /**
  * @description 读取 cookie
- * @since 1.1.0
+ * @since 1.2.0
  * @returns {Record<string,string>} cookie
  */
 export function readCookie() {
+	const ckFile = fs.readFileSync(path.resolve(pathList.http, "cookie.json"), "utf8");
 	return JSON.parse(ckFile);
 }
 
 /**
  * @description 读取 cookie 某项值
- * @since 1.1.0
+ * @since 1.2.0
  * @param {string} itemKey 项名称
  * @returns {string} 项值
  */
 export function readCookieItem(itemKey){
+	const ckFile = fs.readFileSync(path.resolve(pathList.http, "cookie.json"), "utf8");
 	const ck = JSON.parse(ckFile);
 	return ck[itemKey];
 }
