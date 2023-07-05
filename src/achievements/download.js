344e38700438ba745760e3caf78348cc8edd05b1
BTMuli <BT-Muli@outlook.com>
2023-07-05 08:18:26 +0800

    ⬆️ 适应repo更新调整路径

--- src/achievements/download.js
+++ src/achievements/download.js
@@ -2,7 +2,7 @@
  * @file achievements download.js
  * @description 下载所需的 JSON 文件
  * @author BTMuli<bt-muli@outlook.com>
- * @since 1.1.0
+ * @since 1.2.0
  */
 
 // Node
@@ -20,7 +20,7 @@ const dataList = [
 	{
 		name: "胡桃-成就",
 		repo: pathList.repo.SH,
-		file: "Output/CHS/Achievement.json",
+		file: "Genshin/CHS/Achievement.json",
 		tree: "main",
 		savePath: path.resolve(srcDir, "SnapHutao.json"),
 	},
@@ -30,15 +30,17 @@ const dataList = [
 		file: "src/data/achievement/zh.json",
 		tree: "main",
 		savePath: path.resolve(srcDir, "Paimon.json"),
-	}
+	},
 ];
 
 // 检测目录是否存在
 dirCheck(srcDir);
 
-await Promise.allSettled(dataList.map(async data => {
-	defaultLogger.info(`[成就][下载] 开始下载 ${data.name} 文件`);
-	await gitDownload(data.repo, data.file, data.savePath);
-}));
+await Promise.allSettled(
+	dataList.map(async (data) => {
+		defaultLogger.info(`[成就][下载] 开始下载 ${data.name} 文件`);
+		await gitDownload(data.repo, data.file, data.savePath);
+	})
+);
 
 defaultLogger.info("[成就][下载] download.js 运行完成");
