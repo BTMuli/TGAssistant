dbd8f8a7732bb51bda81d651704d4713b93e5f11
BTMuli <BT-Muli@outlook.com>
2023-07-05 08:28:35 +0800

    🎨 优化输出，下载失败时可以手动 copy(

--- src/tools/gitDownload.js
+++ src/tools/gitDownload.js
@@ -2,14 +2,14 @@
  * @file tools gitDownload.js
  * @description 下载 GitHub 仓库的文件
  * @author BTMuli<bt-muli@outlook.com>
- * @since 1.1.0
+ * @since 1.2.0
  */
 
 // Node
 import axios from "axios";
 import { writeFileSync } from "node:fs";
 // TGAssistant
-import { defaultLogger,consoleLogger } from "./logger.js";
+import { defaultLogger, consoleLogger } from "./logger.js";
 
 /**
  * @description 获取 GitHub 仓库的文件对应下载地址
@@ -25,7 +25,7 @@ function getDownloadUrl(repo, file, tree = "master") {
 
 /**
  * @description 下载 GitHub 仓库的文件
- * @since 1.1.0
+ * @since 1.2.0
  * @param {string} repo 仓库地址
  * @param {string} file 文件路径
  * @param {string} savePath 保存路径
@@ -35,14 +35,16 @@ async function gitDownload(repo, file, savePath) {
 	consoleLogger.info(`[gitDownload] 开始下载 ${file}`);
 	const downloadUrl = getDownloadUrl(repo, file);
 	try {
-		await axios.get(downloadUrl, {
-			responseType: "text",
-		}).then((res) => {
-			writeFileSync(savePath, res.data);
-			defaultLogger.info(`[gitDownload] ${file} 下载完成`);
-		});
+		await axios
+			.get(downloadUrl, {
+				responseType: "text",
+			})
+			.then((res) => {
+				writeFileSync(savePath, res.data);
+				defaultLogger.info(`[gitDownload] ${file} 下载完成`);
+			});
 	} catch (error) {
-		defaultLogger.error(`[gitDownload] ${file} 下载失败`);
+		defaultLogger.error(`[gitDownload] ${downloadUrl} 下载失败`);
 	}
 }
 
