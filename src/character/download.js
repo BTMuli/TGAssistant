a0c670f55f9c75438643c69c3dfa2ee213a0be6f
BTMuli <BT-Muli@outlook.com>
2023-07-17 12:33:41 +0800

    🎨 增加数据源

--- src/character/download.js
+++ src/character/download.js
@@ -1,8 +1,8 @@
 /**
  * @file character download.js
  * @description 下载角色原始JSON跟图像
- * @author BTMuli<bt-muli@outlook.com>
- * @since 1.1.0
+ * @author BTMuli <bt-muli@outlook.com>
+ * @since 1.3.0
  */
 
 // Node
@@ -14,7 +14,7 @@ import sharp from "sharp";
 import pathList, { AMBER_VH } from "../../root.js";
 import { consoleLogger, defaultLogger } from "../tools/logger.js";
 import { dirCheck, fileExist } from "../tools/utils.js";
-
+import gitDownload from "../tools/gitDownload.js";
 
 defaultLogger.info("[角色][下载] 开始执行 download.js");
 
@@ -24,14 +24,16 @@ const srcJsonDir = path.resolve(pathList.src.json, "character");
 dirCheck(srcImgDir);
 dirCheck(srcJsonDir);
 
-const mysUrl = "https://api-static.mihoyo.com/common/blackboard/ys_obc/v1/home/content/list?app_sn=ys_obc&channel_id=189";
+const mysUrl =
+  "https://api-static.mihoyo.com/common/blackboard/ys_obc/v1/home/content/list?app_sn=ys_obc&channel_id=189";
 const amberUrl = `https://api.ambr.top/v2/chs/avatar?vh=${AMBER_VH}`;
 const amberSavePath = path.resolve(srcJsonDir, "amber.json");
 const mysSavePath = path.resolve(srcJsonDir, "mys.json");
+const hutaoSavePath = path.resolve(srcJsonDir, "hutao.json");
 
 // 下载JSON
 try {
-	await axios.get(amberUrl).then(res => {
+	await axios.get(amberUrl).then((res) => {
 		const dataGet = res.data["data"];
 		fs.writeFileSync(amberSavePath, JSON.stringify(dataGet, null, 2));
 		defaultLogger.info("[角色][下载] amber.json 下载完成");
@@ -41,9 +43,8 @@ try {
 	defaultLogger.error(error.message);
 }
 
-
 try {
-	await axios.get(mysUrl).then(res => {
+	await axios.get(mysUrl).then((res) => {
 		const dataGet = res.data["data"]["list"][0]["children"][0]["list"];
 		fs.writeFileSync(mysSavePath, JSON.stringify(dataGet, null, 2));
 		defaultLogger.info("[角色][下载] mys.json 下载完成");
@@ -53,6 +54,12 @@ try {
 	defaultLogger.error(error.message);
 }
 
+try {
+	await gitDownload(pathList.repo.SH, "Genshin/CHS/Avatar.json", hutaoSavePath);
+} catch (error) {
+	defaultLogger.error("[角色][下载] hutao.json 下载失败");
+	defaultLogger.error(error.message);
+}
 
 // 下载图片
 const count = {
@@ -63,19 +70,22 @@ const count = {
 };
 const amberJson = JSON.parse(fs.readFileSync(amberSavePath, "utf-8"));
 const amberKeys = Object.keys(amberJson["items"]);
-await Promise.allSettled(amberKeys.map(async (key) => {
+for (const key of amberKeys) {
 	count.total++;
 	const item = amberJson["items"][key];
 	const url = getAmberUrl(item["icon"]);
 	const savePath = path.resolve(srcImgDir, `${key}.png`);
 	if (!fileExist(savePath)) {
+		consoleLogger.info(`[角色][下载][${key}] 开始下载图像`);
 		await downloadImg(url, savePath, key);
 	} else {
 		count.skip++;
 		consoleLogger.mark(`[角色][下载][${key}] 图像已存在，跳过下载`);
 	}
-}));
-defaultLogger.info(`[角色][下载] 下载完成，共 ${count.total} 张图片，成功 ${count.success} 张，失败 ${count.fail} 张，跳过 ${count.skip} 张`);
+}
+defaultLogger.info(
+	`[角色][下载] 下载完成，共 ${count.total} 张图片，成功 ${count.success} 张，失败 ${count.fail} 张，跳过 ${count.skip} 张`,
+);
 defaultLogger.info("[角色][下载] download.js 执行完毕");
 
 // 用到的函数
@@ -100,13 +110,15 @@ function getAmberUrl(pre) {
  */
 async function downloadImg(url, savePath, index) {
 	try {
-		await axios.get(url, {
-			responseType: "arraybuffer",
-		}).then(res => {
-			defaultLogger.info(`[角色][下载][${index}] 图像下载完成`);
-			sharp(res.data).png().toFile(savePath);
-			count.success++;
-		});
+		await axios
+			.get(url, {
+				responseType: "arraybuffer",
+			})
+			.then((res) => {
+				defaultLogger.info(`[角色][下载][${index}] 图像下载完成`);
+				sharp(res.data).png().toFile(savePath);
+				count.success++;
+			});
 	} catch (error) {
 		count.fail++;
 		defaultLogger.error(`[角色][下载][${index}] ${savePath} 下载失败`);
