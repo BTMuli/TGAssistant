e1afc94e57e568863537e63c920ff7a2564c7594
BTMuli <BT-Muli@outlook.com>
2023-11-09 17:56:30 +0800

    ✨ 从数据库中读取 json 更新

--- web/utils/readCookie.ts
+++ web/utils/readCookie.ts
@@ -4,14 +4,17 @@
  * @since 2.0.0
  */
 
+import os from "node:os";
 import path from "node:path";
 
+import DataBase from "better-sqlite3";
 import fs from "fs-extra";
 
 import { getProjDataPath } from "../../core/utils/getBasePaths.ts";
 
 const dataDir = getProjDataPath("data");
-const cookiePath = path.join(dataDir, "cookie.json");
+const cookiePath = path.join(dataDir, "http", "cookie.json");
+const cookieDB = path.join(os.homedir(), "AppData", "Roaming", "TeyvatGuide", "TeyvatGuide.db");
 
 /**
  * @description 读取 cookie
@@ -30,4 +33,17 @@ export function readCookie(): Record<TGWeb.Constant.CookieType, string> {
  */
 export function readCookieItem(cookieType: TGWeb.Constant.CookieType): string {
   return readCookie()[cookieType];
+}
+
+/**
+ * @description 从数据库中读取 cookie，然后写入文件
+ * @since 2.0.0
+ * @return {string} cookie-db
+ */
+export function flushDB(): void {
+  const db = new DataBase(cookieDB);
+  db.pragma("journal_mode = WAL");
+  const res = <{ value: string }>db.prepare("SELECT value FROM AppData WHERE key = 'cookie'").get();
+  db.close();
+  fs.writeJSONSync(cookiePath, JSON.parse(res.value), { spaces: 2 });
 }
