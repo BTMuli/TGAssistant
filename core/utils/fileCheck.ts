223b96165b79aeb33a0ab7e44026358499688224
BTMuli <BT-Muli@outlook.com>
2023-12-15 00:20:32 +0800

    ♻️ 增加重载

--- core/utils/fileCheck.ts
+++ core/utils/fileCheck.ts
@@ -36,7 +36,7 @@ function getDeepValue(obj: object): string[] {
   let res: string[] = [];
   for (const val of Object.values(obj)) {
     if (typeof val === "object") {
-      res = res.concat(getDeepValue(val));
+      res = res.concat(getDeepValue(<object>val));
     } else if (typeof val === "string") {
       res.push(val);
     }
@@ -46,14 +46,18 @@ function getDeepValue(obj: object): string[] {
 
 /**
  * @description 给定 object，检测其最下游的属性对应的文件/目录是否存在
- * @todo 与 fileCheck 重载
  * @param {object} obj 要检查的 object
  * @param {boolean} isDir 是否为目录
- * @returns {void}
+ * @returns {void|boolean}
  */
-export function fileCheckObj(obj: object, isDir: boolean = true): void {
+export function fileCheckObj(obj: object, isDir: false): boolean;
+export function fileCheckObj(obj: object, isDir?: true): void;
+export function fileCheckObj(obj: object, isDir: boolean = true): boolean | undefined {
   const valueArr = getDeepValue(obj);
+  let res;
   for (const value of valueArr) {
-    fileCheck(value, isDir);
+    res = fileCheck(value, isDir);
+    if (!isDir && !res) return false;
   }
+  if (!isDir) return true;
 }
