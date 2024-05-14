e5a23f03ae2209ebb6c0efacd25f27396d6cd939
目棃 <BT-Muli@outlook.com>
2024-05-15 01:14:25 +0800

    ✅ 完善名片解析
    https://github.com/BTMuli/TeyvatGuide/issues/110

--- core/utils/parseNamecard.ts
+++ core/utils/parseNamecard.ts
@@ -1,38 +1,48 @@
 /**
  * @file core/utils/parseNamecard.ts
  * @description 名片描述解析器
- * @since 2.0.0
+ * @since 2.2.0
  */
 
 import * as fs from "fs-extra";
 
 import { getAppDirPath } from "./getBasePaths.ts";
+import * as console from "node:console";
 
 /**
  * @description 解析名片描述
- * @since 2.0.0
+ * @since 2.2.0
  * @param {string} desc 名片描述
+ * @param {number} index 名片索引
  * @return {string} 解析后的描述
  */
-export function parseNamecard(desc: string): string {
-  let array = [];
-  if (desc.startsWith("名片纹饰。「")) {
+export function parseNamecard(desc: string, index: number): string {
+  const array = [];
+  if (desc.startsWith("名片纹饰。「") && desc.endsWith("」")) {
     array.push("名片纹饰。");
     const reg = /「.+?」/g;
     const match = desc.match(reg);
+    console.log(match);
     if (match !== null) {
       for (const item of match) {
-        if (item.length <= 32) {
+        if (item.length <= 34) {
           array.push(item);
         } else {
           array.push("「");
-          array.push(...parseDesc(item.slice(1, -1)));
-          array.push("」");
+          array.push(...parseDesc(item.slice(1, -1), index, true));
+          const maxLength = Math.max(...array.map((item) => item.length));
+          array.push("  ".repeat(maxLength - 4) + "」");
         }
       }
     }
   } else {
-    array = parseDesc(desc);
+    array.push("名片纹饰。");
+    const content = desc.slice(5);
+    if (content.length <= 32) {
+      array.push(content);
+    } else {
+      array.push(...parseDesc(content, index));
+    }
   }
   const res = array.join("\n");
   if (!res.endsWith("\n")) return res + "\n";
@@ -42,23 +52,30 @@ export function parseNamecard(desc: string): string {
 /**
  * @function parseDesc
  * @description 解析名片描述
- * @since 2.0.0
+ * @since 2.2.0
  * @param {string} desc 名片描述
+ * @param {number} index 名片索引
+ * @param {boolean} inQuote 是否在引号内
  * @return {string[]} 解析后的描述
  */
-function parseDesc(desc: string): string[] {
+function parseDesc(desc: string, index: number, inQuote: boolean = false): string[] {
   let res = desc.replace(/。/g, "。\n");
   res = res.replace(/；/g, "；\n");
-  res = res.replace(/：/g, "：\n");
-  res = res.replace(/？/g, "？\n");
+  if (index !== 187) {
+    res = res.replace(/：/g, "：\n");
+    res = res.replace(/？/g, "？\n");
+  } else {
+    res = res.replace("时候，", "时候，\n");
+    res = res.replace("。\n」", "。」");
+  }
   if (!desc.includes("！」")) {
     res = res.replace(/！/g, "！\n");
   }
   res = res.replace(/…/g, "…\n");
   const match = res.split("\n");
-  const array: string[] = [];
+  let array: string[] = [];
   for (const item of match) {
-    if (item.length > 0 && item.length <= 34) {
+    if (item.length > 0 && item.length <= 32) {
       array.push(item);
     } else {
       const match2 = item.replace(/，/g, "，\n").split("\n");
@@ -67,18 +84,21 @@ function parseDesc(desc: string): string[] {
       }
     }
   }
+  if (inQuote) array = array.map((item) => `  ${item}`);
   return array;
 }
 
 /**
  * @description 根据名称获取名片数据
- * @since 2.0.0
+ * @since 2.2.0
  * @param {string} name 名片名称
  * @return {TGACore.Components.Namecard.ConvertData|false} 名片数据
  */
 export function getNamecardByName(name: string): TGACore.Components.Namecard.ConvertData | false {
   const file = getAppDirPath("data", "namecard").out;
-  const data: TGACore.Components.Namecard.ConvertData[] = fs.readJsonSync(`${file}/namecard.json`);
+  const data: TGACore.Components.Namecard.ConvertData[] = fs.readJsonSync(
+    `${file}/app/namecard.json`,
+  );
   const namecard = data.find((item) => item.name === name);
   if (namecard === undefined) return false;
   return namecard;
--- test/parseNamecard.test.ts
+++ test/parseNamecard.test.ts
@@ -1,12 +1,12 @@
 /**
  * @file test/parseNamecard.test.ts
  * @description parseNamecard 测试文件
- * @since 2.0.0
+ * @since 2.2.0
  */
 
-import { test, expect, describe } from "vitest";
+import { describe, expect, test } from "vitest";
 
-import { parseNamecard, getNamecardByName } from "../core/utils/parseNamecard.ts";
+import { getNamecardByName, parseNamecard } from "../core/utils/parseNamecard.ts";
 
 describe("parseNamecard 测试", () => {
   test("测试 稻妻·雷电之纹", () => {
@@ -14,7 +14,7 @@ describe("parseNamecard 测试", () => {
     const namecard = getNamecardByName("稻妻·雷电之纹");
     expect(namecard).not.toBe(false);
     if (namecard === false) return;
-    const parsed = parseNamecard(namecard.desc);
+    const parsed = parseNamecard(namecard.desc, namecard.index);
     console.log("解析结果：", JSON.stringify(parsed));
     const check = "名片纹饰。\n大御所将军的纹样，亦即麾下军势之旗印，「雷之三重巴」。\n";
     console.log("预期结果：", JSON.stringify(check));
@@ -25,10 +25,13 @@ describe("parseNamecard 测试", () => {
     const namecard = getNamecardByName("枫丹·奇械");
     expect(namecard).not.toBe(false);
     if (namecard === false) return;
-    const parsed = parseNamecard(namecard.desc);
+    const parsed = parseNamecard(namecard.desc, namecard.index);
     console.log("解析结果：", JSON.stringify(parsed));
     const check =
-      "名片纹饰。\n枫丹发条机关通用能源模块的标志。\n关于要不要注册为商标曾经有过大讨论。\n总之呼吁大家不要随意使用。\n";
+      "名片纹饰。\n" +
+      "枫丹发条机关通用能源模块的标志。\n" +
+      "关于要不要注册为商标曾经有过大讨论。\n" +
+      "总之呼吁大家不要随意使用。\n";
     console.log("预期结果：", JSON.stringify(check));
     expect.soft(parsed).toStrictEqual(check);
   });
@@ -37,7 +40,7 @@ describe("parseNamecard 测试", () => {
     const namecard = getNamecardByName("须弥·大梦");
     expect(namecard).not.toBe(false);
     if (namecard === false) return;
-    const parsed = parseNamecard(namecard.desc);
+    const parsed = parseNamecard(namecard.desc, namecard.index);
     console.log("解析结果：", JSON.stringify(parsed));
     const check = "名片纹饰。\n「新芽萌发的时节尚未到来，请小心将我藏起，在草木亦流连的梦境。」\n";
     console.log("预期结果：", JSON.stringify(check));
@@ -48,10 +51,15 @@ describe("parseNamecard 测试", () => {
     const namecard = getNamecardByName("成就·轨迹");
     expect(namecard).not.toBe(false);
     if (namecard === false) return;
-    const parsed = parseNamecard(namecard.desc);
+    const parsed = parseNamecard(namecard.desc, namecard.index);
     console.log("解析结果：", JSON.stringify(parsed));
     const check =
-      "名片纹饰。\n「\n至高的王上啊，深秘院为你呈上预测的结果：\n双星早已被大地的引力捕获…\n经历了漫长的归回周期，如今它们的轨迹将要再度交错。\n」\n";
+      "名片纹饰。\n" +
+      "「\n" +
+      "  至高的王上啊，深秘院为你呈上预测的结果：\n" +
+      "  双星早已被大地的引力捕获…\n" +
+      "  经历了漫长的归回周期，如今它们的轨迹将要再度交错。\n" +
+      "                                              」\n";
     console.log("预期结果：", JSON.stringify(check));
     expect.soft(parsed).toStrictEqual(check);
   });
@@ -60,10 +68,16 @@ describe("parseNamecard 测试", () => {
     const namecard = getNamecardByName("成就·永续");
     expect(namecard).not.toBe(false);
     if (namecard === false) return;
-    const parsed = parseNamecard(namecard.desc);
+    const parsed = parseNamecard(namecard.desc, namecard.index);
     console.log("解析结果：", JSON.stringify(parsed));
     const check =
-      "名片纹饰。\n「\n于螺丝与齿轮中断裂的梦已无人听取，\n唯有不知疲倦的机关舞者，\n仍用似轻羽般优雅落下的脚步，\n日以继夜叩问过往的回音。\n」\n";
+      "名片纹饰。\n" +
+      "「\n" +
+      "  于螺丝与齿轮中断裂的梦已无人听取，\n" +
+      "  唯有不知疲倦的机关舞者，\n" +
+      "  仍用似轻羽般优雅落下的脚步，\n" +
+      "  日以继夜叩问过往的回音。\n" +
+      "                              」\n";
     console.log("预期结果：", JSON.stringify(check));
     expect.soft(parsed).toStrictEqual(check);
   });
@@ -72,7 +86,7 @@ describe("parseNamecard 测试", () => {
     const namecard = getNamecardByName("成就·敲针");
     expect(namecard).not.toBe(false);
     if (namecard === false) return;
-    const parsed = parseNamecard(namecard.desc);
+    const parsed = parseNamecard(namecard.desc, namecard.index);
     console.log("解析结果：", JSON.stringify(parsed));
     const check = "名片纹饰。\n「既然这些鱼都不能吃，何苦敲针做钩呢？」\n「这你就不懂了吧。」\n";
     console.log("预期结果：", JSON.stringify(check));
@@ -83,10 +97,27 @@ describe("parseNamecard 测试", () => {
     const namecard = getNamecardByName("香菱·出锅");
     expect(namecard).not.toBe(false);
     if (namecard === false) return;
-    const parsed = parseNamecard(namecard.desc);
+    const parsed = parseNamecard(namecard.desc, namecard.index);
     console.log("解析结果：", JSON.stringify(parsed));
     const check =
-      "名片纹饰。\n快速而且准确地读出下面的话：\n「出锅锅巴！」、「锅巴出锅！」、「锅巴出击！」、「出击锅巴！」\n";
+      "名片纹饰。\n" +
+      "快速而且准确地读出下面的话：\n" +
+      "「出锅锅巴！」、「锅巴出锅！」、「锅巴出击！」、「出击锅巴！」\n";
+    console.log("预期结果：", JSON.stringify(check));
+    expect.soft(parsed).toStrictEqual(check);
+  });
+  test("测试 阿蕾奇诺·法度", () => {
+    console.log("测试 阿蕾奇诺·法度");
+    const namecard = getNamecardByName("阿蕾奇诺·法度");
+    expect(namecard).not.toBe(false);
+    if (namecard === false) return;
+    const parsed = parseNamecard(namecard.desc, namecard.index);
+    console.log("解析结果：", JSON.stringify(parsed));
+    const check =
+      "名片纹饰。\n" +
+      "「父亲」大人为壁炉之家订立了严苛的法度，\n" +
+      "当中有些条目甚至有些不近人情。\n" +
+      "熟悉她的人，或许能借由这些规则，隐约勾勒出她从不轻言的理想吧。\n";
     console.log("预期结果：", JSON.stringify(check));
     expect.soft(parsed).toStrictEqual(check);
   });
