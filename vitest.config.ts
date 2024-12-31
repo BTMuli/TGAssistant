e52920de7807fe4d2197c5425b6e622db6f6ee50
目棃 <BT-Muli@outlook.com>
2024-12-31 10:16:56 +0800

    ⬆️ 更新依赖

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,8 +6,4 @@
 
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  test: {
-    include: ["test/*.test.ts"],
-  },
-});
+export default defineConfig({ test: { include: ["test/*.test.ts"] } });
